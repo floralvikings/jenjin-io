46b6849e0b66f96455c0cc2ad2d1f592d4a4d1bc
Caleb Brinkman <floralvikings@gmail.com>
2015-11-05 18:19:15 -0600

    Replace GsonMessageReaderTest with GsonReaderSpec

---
+++ src/test/groovy/com/jenjinstudios/io/serialization/GsonReaderSpec.groovy
@@ -0,0 +1,32 @@
+package com.jenjinstudios.io.serialization;
+
+import spock.lang.Specification
+
+import java.nio.ByteBuffer
+
+public class GsonReaderSpec extends Specification {
+    def "GsonMessageReader should be able to correctly deserialize valid JSON into Adapted Message"() {
+        given:
+            def json = '{"class":"com.jenjinstudios.io.serialization.TestMessage","fields":{"name":"foo"}}'
+            // Have to do a little bit of hackery to get a proper byte-array (have to include length of string)
+            def jsonBytes = json.getBytes("UTF-8")
+            def lengthBytes = ByteBuffer.allocate(2).putChar((char) jsonBytes.length).array()
+            def bytes = new byte[jsonBytes.length + lengthBytes.length]
+            System.arraycopy(lengthBytes, 0, bytes, 0, lengthBytes.length);
+            System.arraycopy(jsonBytes, 0, bytes, lengthBytes.length, jsonBytes.length)
+
+        and:
+            def inputStream = new ByteArrayInputStream(bytes)
+            def reader = new GsonMessageReader(inputStream)
+
+        when:
+            def message = reader.read()
+
+        then:
+            message instanceof AdaptedMessage
+            ((AdaptedMessage) message).getName().equals("foo");
+
+        cleanup:
+            reader.close()
+    }
+}
