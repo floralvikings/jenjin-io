2456b993bc46dea95ae7b6cb8ddce1ef4bacc1ad
Caleb Brinkman <floralvikings@gmail.com>
2015-11-11 17:59:12 -0600

    Last of the specs and stubs for now

--- src/main/java/com/jenjinstudios/io/server/ServerBuilder.java
+++ src/main/java/com/jenjinstudios/io/server/ServerBuilder.java
@@ -1,10 +1,12 @@
 package com.jenjinstudios.io.server;
 
 import com.jenjinstudios.io.ExecutionContext;
+import com.jenjinstudios.io.connection.Connection;
 import com.jenjinstudios.io.connection.ReusableConnectionBuilder;
 
 import java.net.ServerSocket;
 import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 /**
  * Used to build Server objects.
@@ -66,6 +68,98 @@ public class ServerBuilder
      * @return This ServerBuilder.
      */
     public ServerBuilder withContextualTasks(Iterable<BiConsumer<Server, ExecutionContext>> tasks) {
+        return this;
+    }
+
+    /**
+     * Build a Server which will execute the given callbacks when a Connection is added.
+     *
+     * @param callbacks The callbacks to execute.
+     *
+     * @return This ServerBuilder
+     */
+    public ServerBuilder withConnectionAddedCallbacks(Iterable<Consumer<Connection>> callbacks) {
+        return this;
+    }
+
+    /**
+     * Build a Server which will execute the given callbacks when a Connection is added.
+     *
+     * @param callbacks The callbacks to execute.
+     *
+     * @return This ServerBuilder
+     */
+    @SafeVarargs
+    public final ServerBuilder withConnectionAddedCallbacks(Consumer<Connection>... callbacks) {
+        return this;
+    }
+
+    /**
+     * Build a Server which will execute the given callbacks when a Connection is removed.
+     *
+     * @param callbacks The callbacks to execute.
+     *
+     * @return This ServerBuilder
+     */
+    public ServerBuilder withConnectionRemovedCallbacks(Iterable<Consumer<Connection>> callbacks) {
+        return this;
+    }
+
+    /**
+     * Build a Server which will execute the given callbacks when a Connection is removed.
+     *
+     * @param callbacks The callbacks to execute.
+     *
+     * @return This ServerBuilder
+     */
+    @SafeVarargs
+    public final ServerBuilder withConnectionRemovedCallbacks(Consumer<Connection>... callbacks) {
+        return this;
+    }
+
+    /**
+     * Build a Server which will execute the given callbacks after startup.
+     *
+     * @param callbacks The callbacks to execute.
+     *
+     * @return This ServerBuilder
+     */
+    public ServerBuilder withStartupCallbacks(Iterable<Consumer<Connection>> callbacks) {
+        return this;
+    }
+
+    /**
+     * Build a Server which will execute the given callbacks after startup.
+     *
+     * @param callbacks The callbacks to execute.
+     *
+     * @return This ServerBuilder
+     */
+    @SafeVarargs
+    public final ServerBuilder withStartupCallbacks(Consumer<Connection>... callbacks) {
+        return this;
+    }
+
+    /**
+     * Build a Server which will execute the given callbacks after shutdown.
+     *
+     * @param callbacks The callbacks to execute.
+     *
+     * @return This ServerBuilder
+     */
+    public ServerBuilder withShutdownCallbacks(Iterable<Consumer<Connection>> callbacks) {
+        return this;
+    }
+
+    /**
+     * Build a Server which will execute the given callbacks after shutdown.
+     *
+     * @param callbacks The callbacks to execute.
+     *
+     * @return This ServerBuilder
+     */
+    @SafeVarargs
+    public final ServerBuilder withShutdownCallbacks(Consumer<Connection>... callbacks) {
         return this;
     }
 }
--- src/test/groovy/com/jenjinstudios/io/server/ServerBuilderSpec.groovy
+++ src/test/groovy/com/jenjinstudios/io/server/ServerBuilderSpec.groovy
@@ -42,6 +42,32 @@ public class ServerBuilderSpec extends Specification {
             server != null
     }
 
+    def "ServerBuilder should throw exception if ServerSocket is set when alread extant"() {
+        given: "A ServerBuilder with a ServerSocket"
+            def serverSocket = Mock(ServerSocket)
+            def builder = new ServerBuilder()
+            builder.withServerSocket(serverSocket)
+
+        when: "The ServerSocket is set again"
+            builder.withServerSocket(serverSocket)
+
+        then: "An IllegalStateException should be thrown"
+            thrown(IllegalStateException)
+    }
+
+    def "ServerBuilder should throw exception if ReusableConnectionBuilder is set when alread extant"() {
+        given: "A ServerBuilder with a ReusableConnectionBuilder"
+            def serverSocket = Mock(ReusableConnectionBuilder)
+            def builder = new ServerBuilder()
+            builder.withReusableConnectionBuilder(serverSocket)
+
+        when: "The ReusableConnectionBuilder is set again"
+            builder.withReusableConnectionBuilder(serverSocket)
+
+        then: "An IllegalStateException should be thrown"
+            thrown(IllegalStateException)
+    }
+
     def "ServerBuilder should pass contextual tasks to built server"() {
         given: "A ServerBuilder with a ServerSocket and ReusableConnectionBuilder"
             def connectionBuilder = Mock(ReusableConnectionBuilder)
@@ -52,6 +78,90 @@ public class ServerBuilderSpec extends Specification {
         and: "A callback"
             def callbacks = Mock(Iterable)
             builder.withContextualTasks(callbacks)
+
+        when: "The Server is built"
+            def server = builder.build()
+
+        then: "Server should not be null"
+            server != null
+
+        and: "Callbacks should have been iterated"
+            1 * callbacks.forEach(_)
+    }
+
+    def "ServerBuilder should pass Connection Added callbacks to built server"() {
+        given: "A ServerBuilder with a ServerSocket and ReusableConnectionBuilder"
+            def connectionBuilder = Mock(ReusableConnectionBuilder)
+            def serverSocket = Mock(ServerSocket)
+            def builder = new ServerBuilder()
+            builder.withServerSocket(serverSocket).withReusableConnectionBuilder(connectionBuilder)
+
+        and: "A connection added callback"
+            def callbacks = Mock(Iterable)
+            builder.withConnectionAddedCallbacks(callbacks)
+
+        when: "The Server is built"
+            def server = builder.build()
+
+        then: "Server should not be null"
+            server != null
+
+        and: "Callbacks should have been iterated"
+            1 * callbacks.forEach(_)
+    }
+
+    def "ServerBuilder should pass Connection Removed callbacks to built server"() {
+        given: "A ServerBuilder with a ServerSocket and ReusableConnectionBuilder"
+            def connectionBuilder = Mock(ReusableConnectionBuilder)
+            def serverSocket = Mock(ServerSocket)
+            def builder = new ServerBuilder()
+            builder.withServerSocket(serverSocket).withReusableConnectionBuilder(connectionBuilder)
+
+        and: "A connection removed callback"
+            def callbacks = Mock(Iterable)
+            builder.withConnectionRemovedCallbacks(callbacks)
+
+        when: "The Server is built"
+            def server = builder.build()
+
+        then: "Server should not be null"
+            server != null
+
+        and: "Callbacks should have been iterated"
+            1 * callbacks.forEach(_)
+    }
+
+    def "ServerBuilder should pass Startup callbacks to built server"() {
+        given: "A ServerBuilder with a ServerSocket and ReusableConnectionBuilder"
+            def connectionBuilder = Mock(ReusableConnectionBuilder)
+            def serverSocket = Mock(ServerSocket)
+            def builder = new ServerBuilder()
+            builder.withServerSocket(serverSocket).withReusableConnectionBuilder(connectionBuilder)
+
+        and: "A startup callback"
+            def callbacks = Mock(Iterable)
+            builder.withStartupCallbacks(callbacks)
+
+        when: "The Server is built"
+            def server = builder.build()
+
+        then: "Server should not be null"
+            server != null
+
+        and: "Callbacks should have been iterated"
+            1 * callbacks.forEach(_)
+    }
+
+    def "ServerBuilder should pass Shutdown callbacks to built server"() {
+        given: "A ServerBuilder with a ServerSocket and ReusableConnectionBuilder"
+            def connectionBuilder = Mock(ReusableConnectionBuilder)
+            def serverSocket = Mock(ServerSocket)
+            def builder = new ServerBuilder()
+            builder.withServerSocket(serverSocket).withReusableConnectionBuilder(connectionBuilder)
+
+        and: "A shutdown callback"
+            def callbacks = Mock(Iterable)
+            builder.withShutdownCallbacks(callbacks)
 
         when: "The Server is built"
             def server = builder.build()
