3481f97565766485b9a15f563ef637d033a299ea
Caleb Brinkman <floralvikings@gmail.com>
2015-11-18 17:35:18 -0600

    Add first spec for MultiConnectionBuilder

---
+++ src/test/groovy/com/jenjinstudios/io/connection/MultiConnectionBuilderSpec.groovy
@@ -0,0 +1,48 @@
+package com.jenjinstudios.io.connection
+
+import com.jenjinstudios.io.*
+import spock.lang.Specification
+
+import java.util.function.BiConsumer
+
+public class MultiConnectionBuilderSpec extends Specification {
+
+
+    def "ConnectionBuilder should properly pass callback to connection when built"() {
+        given: "A MultiConnectionBuilder (with necessary properties) and BiConsumer<Connection, Throwable>"
+            def builder = new MultiConnectionBuilder()
+            def ioFactory = Mock(MessageIOFactory)
+            def contextFactory = Mock(ExecutionContextFactory)
+            def reader = Mock(MessageReader)
+            def callback = Mock(BiConsumer)
+            def exception = Mock(IOException)
+            def socket = Mock(Socket)
+
+            contextFactory.createInstance() >> Mock(ExecutionContext)
+            socket.inputStream >> Mock(InputStream)
+            socket.outputStream >> Mock(OutputStream)
+            ioFactory.createWriter(_) >> Mock(MessageWriter);
+            ioFactory.createReader(_) >> reader
+
+        and: "ErrorCallback is set and Connection is built"
+            builder.withErrorCallback(callback)
+                    .withExecutionContextFactory(contextFactory)
+                    .withMessageIOFactory(ioFactory)
+            def connection = builder.build(socket)
+
+        and: "MessageReader reads, exception is thrown"
+            reader.read() >> { throw exception }
+
+        when: "Connection starts"
+            connection.start()
+
+        and: "Wait for threads to run"
+            Thread.sleep(100)
+
+        then: "Callback should be called with exception and connection"
+            1 * callback.accept(connection, exception)
+
+        cleanup: "Close connection"
+            connection.stop()
+    }
+}
