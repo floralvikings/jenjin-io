3484acc659771cbe782e87e6c09bd85477ab76f9
Caleb Brinkman <floralvikings@gmail.com>
2015-11-20 17:08:00 -0600

    Rework integration test

--- src/integration-test/java/com/jenjinstudios/io/JenjinIOTest.java
+++ src/integration-test/java/com/jenjinstudios/io/JenjinIOTest.java
@@ -7,6 +7,7 @@ import com.jenjinstudios.io.serialization.GsonMessageIOFactory;
 import com.jenjinstudios.io.server.Server;
 import com.jenjinstudios.io.server.ServerBuilder;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -36,14 +37,17 @@ public final class JenjinIOTest
     private static final BiConsumer<Server, ExecutionContext> SERVER_CONTEXT_TASK = mock(BiConsumer.class);
     private static final Consumer<ExecutionContext> SERVER_CONN_CONTEXT_TASK = mock(Consumer.class);
 
-    private static final Consumer<Connection> CLIENT_SHUTDOWN_CALLBACK = mock(Consumer.class);
+    private static final Consumer<Connection> CLIENT_00_SHUTDOWN_CALLBACK = mock(Consumer.class);
+    private static final Consumer<Connection> CLIENT_01_SHUTDOWN_CALLBACK = mock(Consumer.class);
     private static final BiConsumer<Connection, Throwable> SERVER_ERROR_CALLBACK = mock(BiConsumer.class);
     private static final Consumer<Connection> SERVER_CONN_SHUTDOWN_CALLBACK = mock(Consumer.class);
     private static final Consumer<Connection> SERVER_CONN_REMOVED_CALLBACK = mock(Consumer.class);
 
-    private static final Consumer<ExecutionContext> CLIENT_CONTEXT_TASK = mock(Consumer.class);
+    private static final Consumer<ExecutionContext> CLIENT_00_CONTEXT_TASK = mock(Consumer.class);
+    private static final Consumer<ExecutionContext> CLIENT_01_CONTEXT_TASK = mock(Consumer.class);
 
-    private static final BiConsumer<Connection, Throwable> CLIENT_ERROR_CALLBACK = mock(BiConsumer.class);
+    private static final BiConsumer<Connection, Throwable> CLIENT_00_ERROR_CALLBACK = mock(BiConsumer.class);
+    private static final BiConsumer<Connection, Throwable> CLIENT_01_ERROR_CALLBACK = mock(BiConsumer.class);
     private static final Consumer<Server> SERVER_SHUTDOWN_CALLBACK = mock(Consumer.class);
 
     private static final int PORT = 51015;
@@ -60,51 +64,64 @@ public final class JenjinIOTest
     @SuppressWarnings("OverlyLongMethod")
     @Test
     public void coreFunctionalityTest() throws IOException {
-        server = buildServer();                                     // Build a server
+        server = buildServer();
         server.start();                                             // Start the server
 
         block(BLOCK_TIME);
         verify(SERVER_STARTUP_CALLBACK).accept(server);             // Verify startup callbacks are invoked
 
-        Connection client01 = buildConnection();                    // Build a client connection
-        client01.start();                                           // Start the connection
+        Connection client00 = buildConnection(0);                   // Build a client connection
+
+        TestMessage testMessage00 = createMessage(0);               // Create a message (param of 0 means no resp)
+        TestMessage testMessage01 = createMessage(1);               // Create a message (param of 2 means expect resp)
+
+        TestMessage testBroadcast = createMessage(0);               // Create a test broadcast
+
+        client00.start();                                           // Start the connection
 
         block(BLOCK_TIME);
         verify(SERVER_CONN_ADDED_CALLBACK).accept(any());           // Verify connection added callbacks are invoked
 
-        TestMessage testMessage01 = createMessage(1);               // Create a message
-        client01.sendMessage(testMessage01);                        // Send the message
+        client00.sendMessage(testMessage00);                        // Send the first message
 
         block(BLOCK_TIME);
-        verify(MESSAGE_EXEC_TEST_CALLBACK).accept(any());           // Verify message was received
+        verify(MESSAGE_EXEC_TEST_CALLBACK).accept(any());           // Verify message was received by server
         verify(SERVER_CONTEXT_TASK).accept(any(), any());           // Verify server execution context task completes
         verify(SERVER_CONN_CONTEXT_TASK).accept(any());             // Verify connection exec context task completes
 
-        client01.stop();                                            // Shutdown the first client
+
+        Connection client01 = buildConnection(1);                   // Build another client connection
+        client01.start();                                           // Start the connection
 
         block(BLOCK_TIME);
-        verify(CLIENT_SHUTDOWN_CALLBACK).accept(client01);          // Verify client shutdown callback is invoked
+        final int i = server.getConnectionCount();
+        Assert.assertEquals("Server should have 2 clients", 2, i);
+        server.broadcast(testBroadcast);                         // Broadcast a Message to all clients (no resp)
+
+        block(BLOCK_TIME);
+        verify(CLIENT_00_CONTEXT_TASK, times(1)).accept(any());     // Verify all clients receive broadcast
+        verify(CLIENT_01_CONTEXT_TASK, times(1)).accept(any());     // Verify all clients receive broadcast
+
+        client00.stop();                                            // Shutdown the first client
+
+        block(BLOCK_TIME);
+        verify(CLIENT_00_SHUTDOWN_CALLBACK).accept(client00);       // Verify client shutdown callback is invoked
         verify(SERVER_ERROR_CALLBACK).accept(any(), any());         // Verify server error callback is invoked
         verify(SERVER_CONN_SHUTDOWN_CALLBACK).accept(any());        // Verify server connection is shutdown
         verify(SERVER_CONN_REMOVED_CALLBACK).accept(any());         // Verify client removed callback is invoked
 
-        Connection client02 = buildConnection();                    // Build another client connection
-        client02.start();                                           // Start the connection
-
-        TestMessage testMessage02 = createMessage(2);               // Create a message
-        client02.sendMessage(testMessage02);                        // Send the message
+        client01.sendMessage(testMessage01);                        // Send the message
 
         block(BLOCK_TIME);
-        verify(CLIENT_CONTEXT_TASK, times(2)).accept(any());        // Verify client receives response
+        verify(CLIENT_01_CONTEXT_TASK, times(2)).accept(any());     // Verify client receives response
 
         block(BLOCK_TIME);
         server.stop();                                              // Stop the server
 
         block(BLOCK_TIME);
-        verify(CLIENT_ERROR_CALLBACK).accept(eq(client02), any());  // Verify client error callback is invoked
-        verify(SERVER_SHUTDOWN_CALLBACK).accept(server);            // Verify shutdown callbacks are invoked.
+        verify(CLIENT_01_ERROR_CALLBACK).accept(eq(client01), any());// Verify client error callback is invoked
+        verify(SERVER_SHUTDOWN_CALLBACK).accept(server);             // Verify shutdown callbacks are invoked.
 
-        server = null;
     }
 
     /**
@@ -151,17 +168,27 @@ public final class JenjinIOTest
         return serverBuilder.build();
     }
 
-    private static Connection<TestExecutionContext> buildConnection() throws IOException {
+    private static Connection<TestExecutionContext> buildConnection(int clientNum) throws IOException {
         SingleConnectionBuilder connectionBuilder = new SingleConnectionBuilder();
 
         Socket socket = new Socket(LOCALHOST, PORT);
 
         connectionBuilder.withMessageIOFactory(new GsonMessageIOFactory())
               .withSocket(socket)
-              .withExecutionContext(new TestExecutionContext())
-              .withContextualTask(CLIENT_CONTEXT_TASK)
-              .withErrorCallback(CLIENT_ERROR_CALLBACK)
-              .withShutdownCallback(CLIENT_SHUTDOWN_CALLBACK);
+              .withExecutionContext(new TestExecutionContext());
+
+
+        if (clientNum == 0) {
+            connectionBuilder
+                  .withContextualTask(CLIENT_00_CONTEXT_TASK)
+                  .withErrorCallback(CLIENT_00_ERROR_CALLBACK)
+                  .withShutdownCallback(CLIENT_00_SHUTDOWN_CALLBACK);
+        } else if (clientNum == 1) {
+            connectionBuilder
+                  .withContextualTask(CLIENT_01_CONTEXT_TASK)
+                  .withErrorCallback(CLIENT_01_ERROR_CALLBACK)
+                  .withShutdownCallback(CLIENT_01_SHUTDOWN_CALLBACK);
+        }
 
         return connectionBuilder.build();
     }
@@ -197,9 +224,9 @@ public final class JenjinIOTest
 
         @Override
         public Message execute(TestExecutionContext context) {
+            MESSAGE_EXEC_TEST_CALLBACK.accept(context);
             TestMessage response = null;
             if (value > 0) {
-                MESSAGE_EXEC_TEST_CALLBACK.accept(context);
                 context.setTestValue(value);
                 response = new TestMessage();
                 response.setValue(value - 1);
