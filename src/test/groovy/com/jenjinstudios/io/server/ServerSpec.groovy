b5f7df4952cbd1bcaecead01a160085bbd22a043
Caleb Brinkman <floralvikings@users.noreply.github.com>
2015-12-02 13:43:17 -0600

    Increase wait time
    CI continues to display unnecessary warning messages

--- src/test/groovy/com/jenjinstudios/io/server/ServerSpec.groovy
+++ src/test/groovy/com/jenjinstudios/io/server/ServerSpec.groovy
@@ -98,7 +98,7 @@ public class ServerSpec extends Specification {
         given: "A ServerSocket which returns a mocked connection"
             def serverSocket = Mock(ServerSocket)
             def socket = Mock(Socket)
-            serverSocket.accept() >>> [socket, {Thread.sleep(10000); null}]
+            serverSocket.accept() >>> [socket, {Thread.sleep(100000); null}]
             def connection = Mock(Connection)
             def connectionBuilder = Mock(MultiConnectionBuilder)
             connectionBuilder.build(socket) >> connection
